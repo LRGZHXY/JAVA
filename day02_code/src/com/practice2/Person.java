94186b0d57f586abe57123a4788fbeb91b1a6f7b
lr <3399075402@qq.com>
2024-03-04 21:51:19 +0800

    note

---
+++ day02_code/src/com/practice2/Person.java
@@ -0,0 +1,18 @@
+package com.practice2;
+
+public class test10 {
+    public static void main(String[] args) {
+        public class Phone{
+            //属性
+            String brand;
+            double price;
+            //行为
+            public void call(){
+                System.out.println("打电话");
+            }
+            public void playGame(){
+                System.out.println("玩游戏");
+            }
+        }
+    }
+}
