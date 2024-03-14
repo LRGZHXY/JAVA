98f49b25bcaaa28069398c281f57ab3542414705
lr <3399075402@qq.com>
2024-03-14 12:30:42 +0800

    javacode

--- day02_code/src/testproject/test3.java
+++ day02_code/src/testproject/test3.java
@@ -1,2 +1,10 @@
-package testproject;public class test3 {
+package testproject;
+
+public class test3 {
+    public static void main(String[] args) {
+
+        StringBuilder sb=new StringBuilder();
+        System.out.println(sb.capacity());//16
+    }
+
 }
