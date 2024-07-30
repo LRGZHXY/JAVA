fd9e99a9446a5dd93a5063830a6e445116ebab80
lr <3399075402@qq.com>
2024-07-30 11:55:39 +0800

    leet

---
+++ LeetCode/src/findContentChildren.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count=0;
+        int i=s.length-1;
+        for(int j=g.length-1;j>=0;j--){
+            if(i>=0&&s[i]>=g[j]){
+                i--;
+                count++;
+            }
+        }
+        return count;
+    }
+}
